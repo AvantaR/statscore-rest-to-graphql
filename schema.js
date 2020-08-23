4f286509770e59b70fd00ff086d362d91b6bd4d5
AvantaR <avantar@gmail.com>
2020-08-23 16:48:21 +0200

    Adds first implementation for events

--- schema.js
+++ schema.js
@@ -1,12 +1,14 @@
 import graphql from 'graphql-compose';
 import sportResolvers from './resolvers/sport.js'
 import venueResolvers from './resolvers/venue.js';
+import eventResolvers from './resolvers/event.js';
 
 const { schemaComposer } = graphql;
 
 schemaComposer.Query.addFields({
     ...sportResolvers,
-    ...venueResolvers
+    ...venueResolvers,
+    ...eventResolvers
 })
 
 export default schemaComposer.buildSchema();
